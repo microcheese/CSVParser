26e38037cbad6940fff1ed8b74a494e4624738ae
microcheese <ernst.reiter@microcheese.de>
2022-01-13 13:31:07 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CSVParser",
-            path: ".",
-            dependencies: []),
+            dependencies: [],
+            path: "."
+        ),
    ]
 )
